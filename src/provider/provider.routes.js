4e2c07c6cb9adf39023d6f3969b7a04fe516dbac
pconsuegra-2023018 <pconsuegra-2023018@kinal.edu.gt>
2025-06-04 22:12:06 -0600

    Update provider.routes.js

--- src/provider/provider.routes.js
+++ src/provider/provider.routes.js
@@ -1,14 +1,21 @@
 import { Router } from "express";
-import { getAllProviders, 
+import { deleteProvider, 
+    getAllProviders, 
     getProviderById, 
     getProviderByName, 
-    saveProvider } from "./provider.controller";
+    saveProvider, 
+    updateProvider} from "./provider.controller";
 
 const api = Router()
 
 api.post('addProvider', saveProvider)
+
 api.get('getProvider',getAllProviders)
 api.get('getProvider/:provider',getProviderByName)
 api.get('getProvider/:id',getProviderById)
+
+api.put('updateProvider/:id',updateProvider)
+
+api.delete('deleteProvider/:id',deleteProvider)
 
 export default api
