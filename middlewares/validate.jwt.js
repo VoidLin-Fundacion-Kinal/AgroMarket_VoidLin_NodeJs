a0f5d6babc3ee15082838b5715fa25a44d810e8a
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-06 12:36:09 -0600

    Create validate.jwt.js

---
+++ middlewares/validate.jwt.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+export const validateJwt = async(req,res,next)=>{
+    try {
+        let secretKey = process.env.SECRET_KEY
+        let { authorization } =   req.headers
+        if(!authorization) return res.status(401).send({message: 'It does not have a Token.'})
+        let user = jwt.verify(authorization, secretKey)
+        req.user = user
+        next()
+    } catch (error) {
+        console.error(error)
+        return res.status(401).send({message: 'Invalid credentials'})
+    }
+}
