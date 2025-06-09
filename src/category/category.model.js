23514bf191f6778299614ab0099f0f434f048857
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-09 09:37:45 -0600

    Create category.model.js

---
+++ src/category/category.model.js
@@ -0,0 +1,20 @@
+import {Schema, model} from 'mongoose'
+
+const categorySchema = Schema(
+    {
+        name:{
+            type: String,
+            required: [true, 'Name is required'],
+            maxLength: [20, `Can't be overcome 20 characters`]
+        },
+        
+        description: {
+            type: String,
+            required: [true, 'Description is required'],
+            maxLength: [50, `Can't be overcome 50 characters`]
+        }
+    },
+    {versionKey: false}
+)
+
+export default model('Category', categorySchema)
