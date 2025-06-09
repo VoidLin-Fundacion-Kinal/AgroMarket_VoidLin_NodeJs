1ebdc9562bd5abbb895db5d4c791c2d4a3e5e947
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-09 09:38:05 -0600

    Create post.model.js

---
+++ src/post/post.model.js
@@ -0,0 +1,81 @@
+import { Schema, model } from 'mongoose'
+
+const postSchema = new Schema({
+    user: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100,
+        minLength: 5
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    description: {
+        type: String,
+        required: true,
+        maxLength: 500,
+        minLength: 5
+    },
+
+    address: {
+        type: String,
+        required: true,
+        maxLength: 75,
+        minLength: 5
+    },
+
+    images: [
+        {
+            type: String
+        }
+    ],
+
+    personalData: {
+        cui: {
+            type: String,
+            required: true,
+            minLength: 13,
+            maxLength: 13
+        },
+
+        nit: {
+            type: String,
+            required: true,
+            minLength: 5,
+            maxLength: 10
+        }
+    },
+
+    comments:[
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+
+            comment:{
+                type: String,
+                maxLength: 300,
+            },
+
+            date:{
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+
+
+}) 
+
+
+export default model('Post', postSchema)
