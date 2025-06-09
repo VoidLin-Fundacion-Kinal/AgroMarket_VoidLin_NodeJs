a44618af0025a56fe7b4911f3434b1c062fa2f4b
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-09 09:37:35 -0600

    Create bill.model.js

---
+++ src/bill/bill.model.js
@@ -0,0 +1,35 @@
+import {Schema, model} from 'mongoose'
+
+const billSchema = new Schema (
+    {
+        cart: {
+            type: Schema.Types.ObjectId,
+            ref: 'Cart',
+            required: true
+        },
+        
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+
+        total: {
+            type: Number,
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: ['paid', 'pendig', 'cancelled'],
+            default: 'pending'
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+export default model('Bill', billSchema)
