0b4f5c7de8b5915bace8ecc44743deee44ca4f6c
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-09 09:38:26 -0600

    Update db.validators.js

--- utils/db.validators.js
+++ utils/db.validators.js
@@ -1,4 +1,11 @@
 import User from '../src/user/user.model.js'
+import Category from './../src/category/category.model.js'
+import Provider from './../src/provider/provider.model.js'
+import Product from './../src/product/product.model.js'
+import { body, param } from 'express-validator';
+import mongoose from 'mongoose';
+
+
 
 export const existUsername = async(username, user, id) =>{
     const alreadyUsername = await User.findOne({username})
@@ -17,18 +24,18 @@ export const existEmail = async (email, user)=>{
 }
 
 export const existCui = async (cui, { req }) => {
-    const alreadyCui = await User.findOne({ "personalData.cui": cui });
+    const alreadyCui = await User.findOne({ "personalData.cui": cui }) 
     if (alreadyCui) {
-        throw new Error('CUI is already taken');
+        throw new Error('CUI is already taken') 
     }
-};
+} 
 
 export const existNit = async (nit, { req }) => {
-    const alreadyNit = await User.findOne({ "personalData.nit": nit });
+    const alreadyNit = await User.findOne({ "personalData.nit": nit }) 
     if (alreadyNit) {
-        throw new Error('NIT is already taken');
+        throw new Error('NIT is already taken') 
     }
-};
+} 
 
 
 export const existPhone = async (phone, user)=>{
@@ -38,6 +45,98 @@ export const existPhone = async (phone, user)=>{
         throw new Error(`Phone ${phone} is already taken`)
     }
 }
+
+export const existCategory = async (name) => {
+    const alreadyCategory = await Category.findOne({ name: name.trim().toLowerCase() });
+    if (alreadyCategory) {
+        console.error(`Category '${name}' is already registered`);
+        throw new Error(`Category '${name}' is already registered`);
+    }
+};
+
+
+// ---------------- Provider validators ----------------
+export const existProvider = async (name) => {
+    const alreadyProvider = await Provider.findOne({ name: name.trim() });
+    if (alreadyProvider) {
+        console.error(`Provider '${name}' is already registered`);
+        throw new Error(`Provider '${name}' is already registered`);
+    }
+};
+
+export const existEmailProvider = async (email) => {
+    const alreadyEmail = await Provider.findOne({ email: email.trim()});
+    if (alreadyEmail) {
+        console.error(`Email '${email}' is already registered`);
+        throw new Error(`Email '${email}' is already registered`);
+    }  
+}
+
+export const existPhoneProvider = async (phone) => {
+    const alreadyPhone = await Provider.findOne({ phone: phone.trim() });
+    if (alreadyPhone) {
+        console.error(`Phone '${phone}' is already registered`);
+        throw new Error(`Phone '${phone}' is already registered`);
+    }
+}
+
+export const isValidObjectId = (value, { req }) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) {
+    throw new Error('Invalid ObjectId');
+  }
+  return true;
+}
+
+
+// ---------------- Product validators ----------------
+
+export const existProduct = async(name) => {
+    const alreadyProduct = await Product.findOne({ name: name.trim()});
+    if (alreadyProduct) {
+        console.error(`Product '${name}' is already registered`);
+        throw new Error(`Product '${name}' is already registered`);
+    }
+}
+
+// ---------------- Publicaciones validators ----------------
+//No se realizan =)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const findUser = async(id)=>{
     try{
