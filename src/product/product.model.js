abfd6c7f105acf8922ba01ff0258065f7a2da0d6
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-09 09:38:11 -0600

    Create product.model.js

---
+++ src/product/product.model.js
@@ -0,0 +1,53 @@
+import {Schema, model} from 'mongoose'
+
+const productSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            maxLength: 30
+        },
+
+        description:{
+            type: String,
+            required: true,
+            maxLength: 50
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+         
+        weigth: {
+            type: String,
+            required: true,
+        },
+        
+        image: 
+            {
+                type: String,
+            }
+        ,
+
+
+        stock: {
+            type: Number,
+            default: 0
+        },
+        
+        provider: {
+            type: Schema.Types.ObjectId,
+            ref: 'Provider',
+            required:true
+        },
+
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true
+        }
+    }
+)
+
+export default model('Product', productSchema)
