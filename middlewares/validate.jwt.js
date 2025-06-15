6327e8b2a4def7859f5ae245af200748a4923f3b
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-06-14 19:40:51 -0600

    Update validate.jwt.js

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -28,3 +28,24 @@ export const validateJwt = async(req, res, next)=>{
         return res.status(401).send({message: 'Invalid credentials'})
     }
 }
+
+export const isAdmin = async(req, res, next)=>{
+    try{
+        const { user } = req
+        if(!user || user.role !== 'ADMINPLATAFORM') return res.status(403).send(
+            {
+                success: false,
+                message: `You don't have access | username ${user.username}`
+            }
+        )
+        next()
+    }catch(err){
+        console.error(err)
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'Error with authorization'
+            }
+        )
+    }
+}
