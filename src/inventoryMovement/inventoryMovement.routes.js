9b75076b1b6cabc4c0eb81085000e9bb86ad11fa
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-06-17 23:18:47 -0600

    Update inventoryMovement.routes.js

--- src/inventoryMovement/inventoryMovement.routes.js
+++ src/inventoryMovement/inventoryMovement.routes.js
@@ -3,22 +3,29 @@ import {Router} from 'express'
 import {
     addInventoryMovement,
     updateInventoryMovement,
-    deleteInventoryMovement,
+    softDeleteInventoryMovement,
     listInventoryMovement,
+    listInventoryMovementActive,
     listInventoryMovementById
 
 } from './inventoryMovement.controller.js'
 
 import {
-    validateJwt
+    validateJwt,
+    isAdmin
 } from '../../middlewares/validate.jwt.js'
-import { addInventoryMovementV, updateInventoryMovementV } from '../../middlewares/validators.js'
+import { 
+            addInventoryMovementV, 
+            updateInventoryMovementV,
+            softDeleteInventoryMovementV
+         } from '../../middlewares/validators.js'
 
 const api = Router()
 
-api.post('/addInventoryMovement', validateJwt,addInventoryMovementV,  addInventoryMovement)
+api.post('/addInventoryMovement', validateJwt, addInventoryMovementV,  addInventoryMovement)
 api.put('/updateInventoryMovement/:id', validateJwt, updateInventoryMovementV, updateInventoryMovement)
-api.delete('/deleteInventoryMovement/:id', validateJwt, deleteInventoryMovement)
-api.get('/listInventoryMovement', validateJwt, listInventoryMovement)
+api.put('/softDeleteInventoryMovement/:id', validateJwt, isAdmin, softDeleteInventoryMovementV, softDeleteInventoryMovement)
+api.get('/listInventoryMovement', validateJwt, isAdmin, listInventoryMovement)
+api.get('/listInventoryMovementActive', validateJwt, listInventoryMovementActive)
 api.get('/listInventoryMovementById/:id', validateJwt, listInventoryMovementById)
 export default api
