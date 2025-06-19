e0b9620d0045e8941f800c39463d1347eab39407
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-06-19 10:10:46 -0600

    Update user.routes.js

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -7,7 +7,8 @@ import {
     softDeleteUser, 
     getAllUser,
     getAllActiveUsers,
-    updateUserLogo
+    updateUserLogo,
+    softDeleteUserByAdmin
 } from './user.controller.js'
 
 import {
@@ -30,5 +31,6 @@ api.put('/softDeleteUser', validateJwt, softDeleteUserV, softDeleteUser)
 api.get('/getAllUser', validateJwt, isAdmin, getAllUser)
 api.get('/getAllActiveUsers', validateJwt, isAdmin, getAllActiveUsers)
 api.get('/listUserById', validateJwt, listUserById)
+api.put('/softDeleteUserByAdmin/:id', validateJwt, isAdmin, softDeleteUserByAdmin)
 
 export default api
